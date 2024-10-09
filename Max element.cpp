c731611fa87372caa269c620b3adbeab83fdb949
Jha18Aditya <adityajha68943@gmail.com>
2024-10-09 08:40:48 +0530

    Update Max element.cpp

--- Max element.cpp
+++ Max element.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 using namespace std;
-
-int findMax(int arr[], int size) {
-    int max = arr[0];
-    for (int i = 1; i < size; ++i) {
+int main() {
+    int arr[] = {1, 5, 37, 9, 2};
+       int max = arr[0];
+    for (int i = 1; i < 5; i++) {
         if (arr[i] > max) {
             max = arr[i];
         }
-    }
-    return max;
-}
+        }
 
-int main() {
-    int arr[] = {1, 5, 3, 9, 2};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    cout << "Maximum element: " << findMax(arr, size) << endl;
+    cout << "Maximum element: " << max << endl;
     return 0;
 }
+        
+
